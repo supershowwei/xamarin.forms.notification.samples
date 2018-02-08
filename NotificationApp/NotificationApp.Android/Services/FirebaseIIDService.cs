f9f8203a6ce6be928b08a95dcf0a6e03e1f00042
johnny <johnny@wantgoo.com>
2018-02-08 14:33:57 +0800

    first commit

---
+++ NotificationApp/NotificationApp.Android/Services/FirebaseIIDService.cs
@@ -0,0 +1,18 @@
+﻿using Android.App;
+using Android.Util;
+using Firebase.Iid;
+
+namespace NotificationApp.Droid.Services
+{
+    [Service]
+    [IntentFilter(new[] { "com.google.firebase.INSTANCE_ID_EVENT" })]
+    public class FirebaseIIDService : FirebaseInstanceIdService
+    {
+        public override void OnTokenRefresh()
+        {
+            var token = FirebaseInstanceId.Instance.Token;
+
+            Log.Debug("NotificationApp", token);
+        }
+    }
+}
